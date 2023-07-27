9cde83ffdc2e0d1b42f317cc28f6dec0ba6a766d
Grant Moore <chinchalinchin@gmail.com>
2023-07-27 11:46:31 -0400

    init gh-pages

--- output.tf
+++
@@ -1,9 +0,0 @@
-ouput "distribution" {
-    value               = { 
-        arn             = aws_cloudfront_distribution.this.arn
-        domain_name     = aws_cloudfront_distribution.this.domain_name
-        hosted_zone_id  = aws_cloudfront_distribution.this.hosted_zone_id
-        id              = aws_cloudfront_distribution.this.id
-
-    }
-}
