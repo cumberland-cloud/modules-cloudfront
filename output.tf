a5e8cc8f3f7181f2db4c08f2e7f32fe99fcbd537
Grant Moore <chinchalinchin@gmail.com>
2023-07-27 11:41:21 -0400

    add outputs

--- output.tf
+++ output.tf
@@ -1,5 +1,9 @@
 ouput "distribution" {
     value               = { 
-        id              = "TODO"
+        arn             = aws_cloudfront_distribution.this.arn
+        domain_name     = aws_cloudfront_distribution.this.domain_name
+        hosted_zone_id  = aws_cloudfront_distribution.this.hosted_zone_id
+        id              = aws_cloudfront_distribution.this.id
+
     }
 }
